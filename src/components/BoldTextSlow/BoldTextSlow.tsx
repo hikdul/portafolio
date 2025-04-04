42dfeb8f7cdcd6f80598b0d1b713363f2ad72576
hikdul <hikdul.lio@gmail.com>
2025-04-04 14:52:22 -0500

    Up[tecnologias]: generando bases de como seran las tecnologias

---
+++ src/components/BoldTextSlow/BoldTextSlow.tsx
@@ -0,0 +1,3 @@
+
+export const BoldTextSlow = (p:{text:string}) =>(<b className="duration-300 text-green-300 group-hover:text-green-500">{p.text}</b>)
+    
---
+++ src/components/Card/CardIT.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react"
+import { DynamicIcon, iconName } from "../DynamicIcon/DynamicIcon"
+
+
+
+interface props{
+    icon:string
+    children: ReactNode | ReactNode[];
+}
+
+const CardIT = ({icon, children}:props) =>{
+    
+    return(
+        <div className="group font-extralight hover:font-medium duration-150 ">
+          <div className="h-0.5 w-[0%] group-hover:w-[60%] duration-500 bg-green-400 mb-10 "></div>
+          <DynamicIcon name={icon as iconName} className="text-8xl bg-transparent text-green-400 mx-3 p-1 rounded duration-300" />
+          <article>
+            {children}
+          </article>
+          <div className="h-0.5 w-[0%] group-hover:w-[90%] duration-500 bg-green-400  mt-10"></div>
+        </div>
+    )
+}
+
+export {
+    CardIT
+}
